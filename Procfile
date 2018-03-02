45d7308476573a2ba79c1f70ccebefcf5392eeab
walshjack00 <walshjack00@gmail.com>
2018-03-02 12:59:54 -0500

    dab dab

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn WweebbComics.wsgi --log-file -
--- WweebbComics/settings.py
+++ WweebbComics/settings.py
@@ -121,8 +121,3 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
